fb4eb35d7d202ba8644e7c981f1acf61bd4f296b
trankhanh040147 <trankhanh040147@gmail.com>
2023-05-05 19:32:44 +0700

    Update ConnectJDBC.java

--- Pandora/src/main/java/vn/pandora/Connection/ConnectJDBC.java
+++ Pandora/src/main/java/vn/pandora/Connection/ConnectJDBC.java
@@ -8,12 +8,12 @@ import vn.pandora.Connection.*;
 public class ConnectJDBC {
 
 	// Connection của Ân
-	private final String serverName = "LAPTOP-EVKJEPF2";
-	private final String dbName = "Pandora";
-	private final String portNumber = "1433";
-	private final String instance = "SQLEXPRESS";
-	private final String userID = "sa";
-	private final String password = "123456";
+		private final String serverName = "LAPTOP-EVKJEPF2";
+		private final String dbName = "Pandora";
+		private final String portNumber = "1433";
+		private final String instance = "SQLEXPRESS";
+		private final String userID = "sa";
+		private final String password = "123456";
 
 	// Connection của P.Khánh
 //		private final String serverName = "localhost";
@@ -24,10 +24,10 @@ public class ConnectJDBC {
 //		private final String password = "040147";
 
 	// Connection của Q.Khánh
-//		private final String serverName = "LAPTOP-EVKJEPF2";
-//		private final String dbName = "QUANLYDETAI";
+//		private final String serverName = "localhost";
+//		private final String dbName = "Pandora";
 //		private final String portNumber = "1433";
-//		private final String instance = "SQLEXPRESS";
+//		private final String instance = "";
 //		private final String userID = "sa";
 //		private final String password = "123456";
 
@@ -39,6 +39,12 @@ public class ConnectJDBC {
 //		private final String userID = "sa";
 //		private final String password = "123456";
 	// Conection của Khang
+//	    private final String serverName = "JESSICA";
+//	    private final String dbName = "Pandora";
+//	    private final String portNumber = "1433";
+//	    private final String instance = "SQLEXPRESS";
+//	    private final String userID = "sa";
+//	    private final String password = "123456";
 	// Connection của Lâm
 //		private final String serverName = "LAMNGUYEN";
 //		private final String dbName = "Pandora";
@@ -46,7 +52,13 @@ public class ConnectJDBC {
 //		private final String instance = "SQLEXPRESS";
 //		private final String userID = "sa";
 //		private final String password = "109339Lam@";
-	// Connection của Quân
+	// Conection của Quân 
+//		private final String serverName = "DESKTOP-UBLSUDG";
+//		private final String dbName = "Pandora";
+//		private final String portNumber = "1433";
+//		private final String instance = "SQLEXPRESS";
+//		private final String userID = "sa";
+//		private final String password = "123456";
 
 	public Connection getConnection() throws Exception {
 		String url = "jdbc:sqlserver://" + serverName + "\\" + instance + ";databaseName=" + dbName;
